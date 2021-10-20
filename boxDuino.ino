e6b8ef1b09ba91f5778e524ffde0de07139e23b7
Andrea Perego <78487344+pegAndre@users.noreply.github.com>
2021-10-20 15:34:57 +0200

    Update boxDuino.ino

--- boxDuino.ino
+++ boxDuino.ino
@@ -1,22 +1,23 @@
-#include <Stepper.h>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             #include <Stepper.h>
+//librerie
 #include <Ultrasonic.h>
+#include <Stepper.h>
 
 #define pressionePin A0
+#define valoreZero 600
+#define valoreUno 900
+#define valoreDue 900
 #define giallo 7
 #define verde 6
 #define rosso 5
 #define blu 4
-#define valoreZero 300
-#define valoreUno 700
-#define valoreDue 700
 #define bottone 3
 
-Stepper myStepper(2048, 11, 9, 10, 8);
 Ultrasonic ultra(12, 13);
+Stepper myStepper(2048, 11, 9, 10, 8);
+
+//setup
 
 void setup() {
-  Serial.begin(115200);
-  
   pinMode(pressionePin, INPUT);
   pinMode(giallo, OUTPUT);
   digitalWrite(giallo, LOW);
@@ -25,87 +26,133 @@ void setup() {
   pinMode(rosso, OUTPUT);
   digitalWrite(rosso, LOW);
   pinMode(blu, OUTPUT);
-  digitalWrite(blu, LOW);
-  
-  attachInterrupt(digitalPinToInterrupt(bottone), segnale, FALLING);
-  
+  digitalWrite(blu, HIGH);
   myStepper.setSpeed(10);
+  attachInterrupt(digitalPinToInterrupt(bottone), segnale, FALLING);
 }
 
+//definizioni variabili
+
 int valorePre = 1023;
-int i = 0;
+int valore;
+int indice = 0;
 int acquisito[3];
 int distance;
 int mancano;
 int posizione;
 boolean verifica = true;
-boolean ledBlu = true;
+boolean inizio = true;
 boolean verificato = false;
 boolean lavoroFinito = false;
-boolean inizio = true;
 
-void acquisizione() {      
-  if (inizio == true) {      
-    // acquisisco valore dal sensore di pressione
-    int valore = analogRead(pressionePin);
-    Serial.println(valore);
-    // controllo che non ci siano errori di acquisizione
+// acquisizione valori letti dal sensore di pressione
+void acquisizione() {
+  if (inizio == true) {
+    delay(200);
+    valore = analogRead(pressionePin);
     if (verifica == true) {
-      // se il valore rispetta i requisiti ovvero cambia la sua crescita e non è un errore della resistenza salvo il dato
+      // salvo dato solo se rispetta i parametri di acquisizione
       if (valore > valorePre && valorePre < 1000) {
-        // led giallo identifica iniziata la rilevazione
-        digitalWrite(giallo, HIGH);
-        // spengo il led blu
-        digitalWrite(blu, LOW);
-        ledBlu = false;
-        // salvo il valore passando come parametri il valore e a che posizione inserirlo
-        salvaValore(i, valorePre);
-        // resetto il controllo
+        ledBluValore(false);
+        ledGialloValore(true);
+        salvaValore(indice, valorePre);
         verifica = false;
       }
-      // riprendo a salvare il valore solo quando la resistenza è tornata al livello iniziale
     } else if (valore > 1000) {
       verifica = true;
     }
-
     valorePre = valore;
   }
 }
 
+// salvo il valore nella poszione corretta
 void salvaValore(int posizione, int dato) {
-  // salvao il dato nella posizione corretta
   acquisito[posizione] = dato;
-
-  // se ho raggiunto le 3 acquisizioni le controllo
-  if (i == 2) {
-    // azzero il contatore
-    i = 0;
-    // fine acquisizione
-    digitalWrite(giallo, LOW);
+  if (indice == 2) {
+    indice = 0;
     autenticazione();
   } else {
-    // aumento la posizione
-    i++;
+    indice++;
   }
 }
 
 void autenticazione() {
   animazione();
-  if (acquisito[0] < valoreZero && acquisito[1] < valoreUno && acquisito[2] < valoreDue) {
-    // se corretta led verde che identifica l'accesso
-    digitalWrite(verde, HIGH);
+  // controllo correttezza codice
+  if (acquisito[0] > valoreZero && acquisito[1] < valoreUno && acquisito[2] < valoreDue) {
+    // codice corretto apro la scatola
+    ledVerdeValore(true);
     verificato = true;
     inizio = false;
   } else {
-    // codice errato, riprendo ad aspettare la sequnza, led rosso
-    digitalWrite(rosso, HIGH);
+    // codice errato mi rimetto in ascolto
+    ledRossoValore(true);
     delay(1200);
-    digitalWrite(rosso, LOW);
-    digitalWrite(blu, HIGH);
-    ledBlu = true;
+    ledRossoValore(false);
+    ledBluValore(true);
   }
 }
 
+// apro il coperchio
+void aproCoperchio() {                           // manca controllo sul tempo !!!!
+  if (verificato == true) {
+    ledGialloValore(true);
+    // leggo la distanza
+    distance = ultra.read();
+    if (distance < 20 && distance > 3) {
+      // mi muovo in base alla distanza corretta
+      myStepper.step(-prendoValore(distance));
+    } else if (distance > 19) {
+      // coperchio aperto, aspetto che l'utente lo voglia chiudere
+      ledGialloValore(false);
+      verificato = false;
+    }
+  }
+}
+
+// prendo il valore lo mappo per esere letto dallo stepper motor
+int prendoValore(int posizioneCoperchio) {
+  mancano = 21 - posizioneCoperchio;
+  mancano = map(mancano, 0, 16, 0, 100);
+  mancano = map(mancano, 0, 360, 0, 2048);
+  return mancano;
+}
+
+// chiudo il coperchio
+void chiudiCoperchio() {                              // manca controllo sul tempo !!!!
+  if (lavoroFinito == true) {
+    ledGialloValore(true);
+    // leggo la distanza
+    distance = ultra.read();
+    if (distance > 5) {
+      // muovo lo stepper motor della distanza corretta
+      myStepper.step(prendoValoreChiudo(distance));
+    } else {
+      // coperchio chiuso mi rimetto in ascolto
+      lavoroFinito = false;
+      inizio = true;
+      ledGialloValore(false);
+      ledBluValore(true);
+      ledVerdeValore(false);
+    }
+  }
+}
+
+// prendo il valore lo mappo per esere letto dallo stepper motor
+int prendoValoreChiudo(int posizioneCoperchioChiuso) {
+  mancano = posizioneCoperchioChiuso - 5;
+  mancano = map(mancano, 0, 16, 0, 115);
+  mancano = map(mancano, 0, 360, 0, 2048);
+  return mancano;
+}
+
+// interrupt, il coperchio si chiuderà
+void segnale() {
+  lavoroFinito = true;
+  delay(1200);
+}
+
+// animazione led prima della fase di autenticazione
 void animazione() {
   digitalWrite(blu, HIGH);
   digitalWrite(rosso, HIGH);
@@ -118,74 +165,44 @@ void animazione() {
   digitalWrite(giallo, LOW);
 }
 
-void aproCoperchio() {           // manca controllo sul tempo !!!!
-  digitalWrite(giallo, HIGH);
-  if (verificato == true) {
-    // acquisiisco la distanza di quanto devo spostarmi
-    posizione = prendoValore();
-    // mi muovo di posizione
-    myStepper.step(-posizione);
-    //controllo se sono in posizione corretta
-    distance = ultra.read();
-    if (distance > 20) {
-      digitalWrite(giallo, LOW);
-      verificato = false;
-    }
-  }
-
-}
-
-int prendoValore() {
-  // leggo dal sensore ultraSuoni
-  distance = ultra.read();
-
-
-  if (distance < 21) {
-    // se la distanza è nel range corretto, calcolo quanto mi manca da spostarmi
-    mancano = 21 - distance;
-    // mappo il valore da cm in gradi dove 115 gradi e il mio range di spostamento massimo
-    mancano = map(mancano, 0, 17, 0, 115);
-    // mappo il valore, ne numeri letti dallo steppr
-    mancano = map(mancano, 0, 360, 0, 2048);
+// accendere o spegnere led blu
+void ledBluValore(boolean veroFalso) {
+  if (veroFalso == true) {
+    digitalWrite(blu, HIGH);
   } else {
-    // non devo spostarmi
-    mancano = 0;
+    digitalWrite(blu, LOW);
   }
-  return mancano;
 }
 
-void chiudoCoperchio() {
-  if (lavoroFinito == true) {
+// accendere o spegnere led giallo
+void ledGialloValore(boolean veroFalso) {
+  if (veroFalso == true) {
     digitalWrite(giallo, HIGH);
-    distance = ultra.read();
-    if (distance > 5) {
-      mancano = distance - 5;
-      mancano = map(mancano, 0, 17, 0, 115);
-      mancano = map(mancano, 0, 360, 0, 2048);
-      myStepper.step(mancano);
-    } else {
-      lavoroFinito = false;
-      digitalWrite(verde, LOW);
-      digitalWrite(giallo, LOW);
-      inizio = true;
-      ledBlu = true;
-    }
+  } else {
+    digitalWrite(giallo, LOW);
   }
 }
 
-void segnale() {
-  lavoroFinito = true;
+// accendere o spegnere led verde
+void ledVerdeValore(boolean veroFalso) {
+  if (veroFalso == true) {
+    digitalWrite(verde, HIGH);
+  } else {
+    digitalWrite(verde, LOW);
+  }
 }
 
+// accendere o spegnere led rosso
+void ledRossoValore(boolean veroFalso) {
+  if (veroFalso == true) {
+    digitalWrite(rosso, HIGH);
+  } else {
+    digitalWrite(rosso, LOW);
+  }
+}
 
 void loop() {
-  //led blu identifica che è pronto ad acquisire
-  if (ledBlu == true) {
-    digitalWrite(blu, HIGH);
-  }
-  // avvio acquisizione
   acquisizione();
   aproCoperchio();
-  chiudoCoperchio();
-  delay(100);
+  chiudiCoperchio();
 }
